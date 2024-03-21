c9e06584b23752cef9aa3197705116ae4fc75d88
Emma Craig <emma.craig@getflowbox.com>
2024-03-21 13:40:28 +0100

    fix tab values in header

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,44 @@
-import React from "react";
-import { Tab, Tabs } from "@mui/material";
-import { Link, useLocation } from "react-router-dom";
-import './Header.css'
+import React from 'react';
+import { Tab, Tabs } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 const Header = () => {
   const { pathname } = useLocation();
 
-  const formattedTitle =
-    pathname.substring(1).charAt(0).toUpperCase() +
-    pathname.substring(1).slice(1);
+  const formattedTitle = () => {
+    if (pathname === '/') return 'Notes';
+
+    return (
+      pathname.substring(1).charAt(0).toUpperCase() +
+      pathname.substring(1).slice(1)
+    );
+  };
+  const title = formattedTitle();
   return (
     <div className="header">
-      <h1 >{formattedTitle}</h1>
+      <h1>{title}</h1>
 
-      <Tabs value={pathname} centered>
-        <Tab label="Notes" value="/notes" to="/notes" component={Link} />
-        <Tab label="Fetch" value="/fetch" to="/fetch" component={Link} />
-        <Tab label="Map" value="/map" to="/map" component={Link} />
-
+      <Tabs
+        value={pathname === '/' ? '/notes' : pathname}
+        centered>
+        <Tab
+          label="Notes"
+          value="/notes"
+          to="/notes"
+          component={Link}
+        />
+        <Tab
+          label="Fetch"
+          value="/fetch"
+          to="/fetch"
+          component={Link}
+        />
+        <Tab
+          label="Map"
+          value="/map"
+          to="/map"
+          component={Link}
+        />
       </Tabs>
     </div>
   );
