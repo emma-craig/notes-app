22416e705ec7ba24f4dee4104e72774b5f669700
Emma Craig <emma.craig@getflowbox.com>
2024-03-21 10:25:27 +0100

    update styles and remove ts

--- src/NoteApp/NoteBody/NoteBody.js
+++ src/NoteApp/NoteBody/NoteBody.js
@@ -1,6 +1,15 @@
-import React from "react";
-import "./../Note/Note.css";
+import React from 'react';
+import './../Note/Note.css';
 const NoteBody = ({ text }) => {
-  return <div className="note__body">{text}</div>;
+  return (
+    <div
+      style={{
+        fontSize: '18px',
+        fontFamily:
+          'Fira Mono, DejaVu Sans Mono, Menlo, Consolas, Liberation Mono, Monaco, Lucida Console, monospace',
+      }}>
+      {text}
+    </div>
+  );
 };
 export default NoteBody;
